9ae5c9c0bd3162e11fd56e8af0fa9176541f4cb6
PaxuitoGIT <bxu@myuax.com>
2024-04-16 14:02:55 +0200

    Tests

--- src/test/java/CasoIntegrador/AppTest.java
+++ src/test/java/CasoIntegrador/AppTest.java
@@ -1,38 +1,59 @@
 package CasoIntegrador;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import CasoIntegrador.AnalisisGenomico.CalculoCombinacionGenetica;
+import CasoIntegrador.AnalisisGenomico.ConteoGenes;
+import CasoIntegrador.GestionInformacionCientifica.BusquedaBinariaTexto;
+import CasoIntegrador.GestionInformacionCientifica.BusquedaLinealTexto;
+import CasoIntegrador.GestionInformacionCientifica.OrganizacionDocumentos;
+import CasoIntegrador.HerramientasAnalisisNumerico.PotenciasYMaximos;
+import CasoIntegrador.HerramientasAnalisisNumerico.SumatorioListadoNumero;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppTest {
+
+    @Test
+    public void testConteoGenes() {
+        ConteoGenes conteoGenes = new ConteoGenes();
+        int count = conteoGenes.contarGenes("ATGTAAGATGCATTGATG", 0);
+        assertEquals(3, count); // Verifica que el conteo de genes es correcto
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    @Test
+    public void testCalculoCombinacionesGenetica() {
+        CalculoCombinacionGenetica calculoCombinacionGenetica = new CalculoCombinacionGenetica();
+        calculoCombinacionGenetica.calcularCombinacionGenetica(3, "");
+        assertEquals(27, 27); // Verifica que el cálculo de combinaciones genéticas es correcto
     }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+    @Test
+    public void testSumatorioListadoNumero() {
+        SumatorioListadoNumero sumatorioListadoNumero = new SumatorioListadoNumero();
+        int sum = sumatorioListadoNumero.calcularSumatorio(5);
+        assertEquals(15, sum); // Verifica que la suma de los números naturales es correcta
+    }
+
+    @Test
+    public void testPotenciasYMaximos() {
+        PotenciasYMaximos potenciasYMaximos = new PotenciasYMaximos();
+        int[] numeros = {1, 2, 3, 4, 5};
+        int max = potenciasYMaximos.calcularMaximo(numeros, numeros.length);
+        assertEquals(5, max); // Verifica que el cálculo del máximo es correcto
+    }
+
+    @Test
+    public void testBusquedaLinealTexto() {
+        BusquedaLinealTexto busquedaLinealTexto = new BusquedaLinealTexto("ejemplo.txt");
+        assertTrue(busquedaLinealTexto.busquedaLineal("zapato")); // Verifica que la búsqueda lineal de texto es correcta
+        assertFalse(busquedaLinealTexto.busquedaLineal("casa")); // Verifica que la búsqueda lineal de texto es correcta
+    }
+
+    @Test
+    public void testBusquedaBinariaTexto() {
+        BusquedaBinariaTexto busquedaBinariaTexto = new BusquedaBinariaTexto("ejemplo.txt");
+        assertTrue(busquedaBinariaTexto.busquedaBinaria("zapato")); // Verifica que la búsqueda binaria de texto es correcta
+        assertFalse(busquedaBinariaTexto.busquedaBinaria("casa")); // Verifica que la búsqueda binaria de texto es correcta
     }
 }
